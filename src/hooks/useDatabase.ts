86abf167450339ea91f358262fe9a74afeaa456f
dazzleHroth <dazzle@dazzlefur.me>
2024-08-23 01:10:03 +0100

    More basic setup of crud functions, while constructor moved to own private function and handles other operands

--- src/hooks/useDatabase.ts
+++ src/hooks/useDatabase.ts
@@ -1,5 +1,11 @@
 import sqlite3 from 'sqlite3'
 
+const SqlString = require('sql-string');
+
+/*****************************************************************************************
+ ****************************** DB Initializer
+ *****************************************************************************************/
+
 const db = new sqlite3.Database('DB.sqlite', (err) => {
     if (err) {
         console.error(err.message);
@@ -8,55 +14,97 @@ const db = new sqlite3.Database('DB.sqlite', (err) => {
 });
 
 
-export function dbInsert(tableName: string, data: Object) {
+/*****************************************************************************************
+ ****************************** LOCAL Functions
+ *****************************************************************************************/
 
-    let columns = Object.keys(data).flat();
-    let values = Object.values(data).flat();
-
-    const query = `INSERT INTO ${tableName} (${columns})
-                   values (${values})`
-
-    console.log(query);
-
-    try {
-        db.run('begin transaction')
-        db.run(query);
-        db.run('commit')
-    } catch (err) {
-        console.error(err);
-        db.run('ROLLBACK')
-    }
-}
-
-export async function dbSelectObject(tableName: string, conditions: Object) {
-    return new Promise((resolve, reject) => {
+/**
+ * @description Takes object of fields and conditions and converts to a where statement string
+ * @param conditions
+ */
+function constructWhereCondition(conditions: Object): string {
 
     let whereStatement = "";
     console.log(conditions)
 
     //TODO: Handle other comparisons than equals
     for (let condition of Object.entries(conditions)) {
-        console.log(condition)
-        whereStatement += `"${condition[0]}" = ${condition[1]} AND`;
+
+        //Check start condition value to see if we defined the comparator, add = to start if we did not
+        switch (condition[1][0]) {
+            case '!':
+            case '<':
+            case '>':
+            case '=':
+                break;
+            default:
+                condition[1] = `= ${condition[1]}`;
+        }
+
+
+        whereStatement += `'${condition[0]}' = ${SqlString.escape(condition[1])} AND`;
     }
 
     //Snap off the last AND
-    whereStatement = whereStatement.substring(0, whereStatement.length - 4)
+    return whereStatement.substring(0, whereStatement.length - 4)
 
-    const query = `SELECT * FROM ${tableName} WHERE ${whereStatement}`;
+}
 
-    console.log(query)
+/*****************************************************************************************
+ ****************************** CRUD Functions
+ *****************************************************************************************/
 
-    try {
-        let result
-        db.get(query, (err, response) => {
 
-            console.log(response)
-            resolve(response);
-            result = response
-        })
-    } catch (e) {
-        reject(e)
-    }
+/**
+ * @description Insert 1 record into the specified table
+ * @param tableName
+ * @param data
+ */
+export async function dbInsert(tableName: string, data: Object) {
+    return new Promise<void>(async (resolve, reject) => {
+
+        let columns = Object.keys(data).flat();
+        let values = SqlString.escape(Object.values(data));
+
+        const query = `INSERT INTO ${tableName} (${columns})
+                       values (${values})` //Escaped above
+
+        try {
+            db.run('begin transaction');
+            db.run(query);
+            db.run('commit');
+            resolve();
+        } catch (err) {
+            console.error(err);
+            db.run('ROLLBACK');
+            reject();
+        }
+    })
+}
+
+
+/**
+ * @description Gets record and casts it
+ * @param tableName
+ * @param conditions
+ */
+export async function dbSelectObject(tableName: string, conditions: Object) {
+    return new Promise((resolve, reject) => {
+
+        const query = `SELECT *
+                       FROM ${tableName}
+                       WHERE ${constructWhereCondition(conditions)}`;
+
+        try {
+            let result
+            db.get(query, (err, response) => {
+
+                console.log(response)
+                resolve(response);
+                result = response
+            })
+        } catch (e) {
+            reject(e)
+        }
     })
 }
