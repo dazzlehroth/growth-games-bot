f0fda7fd0682710fb3058a5927a84ac054d700f8
dazzleHroth <dazzle@dazzlefur.me>
2024-08-23 23:34:08 +0100

    Fixed method using new get player object method, also figured out how to get a usertag and NOT ping them

--- src/commands/util/getHeight.ts
+++ src/commands/util/getHeight.ts
@@ -1,5 +1,6 @@
 import {CommandInteraction, SlashCommandBuilder} from "discord.js";
 import {dbSelectRowSingle} from "../../hooks/useDatabase";
+import {getPlayerObject} from "../../hooks/useDBObjects";
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -7,14 +8,14 @@ module.exports = {
         .setDescription('Replies with current height'),
     async execute(interaction: CommandInteraction): Promise<void> {
 
-        const userId = interaction.user.id;
-
-        let user:any = await dbSelectRowSingle('players', {discord_user_id: userId});
-
-        if (user === null || user === undefined) {
+        const userID = interaction.user.id;
+        const guildID = interaction.guild!.id;
+        try  {
+            const player = await getPlayerObject(userID, guildID)
+            await interaction.reply({content: `Your current height is ${player.height} "Generic Height Units ${interaction.member!.user}`, allowedMentions:{parse: []}})
+        } catch {
             await interaction.reply('Player Not found')
-        } else {
-            await interaction.reply(`Your current height is ${user.height} "Generic Height Units"`)
+
         }
 
 
