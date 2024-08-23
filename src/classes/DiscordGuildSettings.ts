ef8757b0a21f318c9821107863e4910de232dd97
dazzleHroth <dazzle@dazzlefur.me>
2024-08-23 01:10:03 +0100

    Renamed for clarity

---
+++ src/classes/DiscordGuildSettings.ts
@@ -0,0 +1,20 @@
+export class DiscordGuildSettings {
+
+
+    id = 0;
+    discordGuidId = 0;
+    maxHeight = null
+    minHeight = null
+
+
+
+
+    async populateSettingsFromDB(){
+
+
+
+    }
+
+
+
+}
