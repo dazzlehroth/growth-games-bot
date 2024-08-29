c0cfbe8e613bf22b9a5d27f746f833919abb12dc
dazzleHroth <dazzle@dazzlefur.me>
2024-08-30 00:13:16 +0100

    Replies ephemerally unless share option is set to true (default false)

--- src/commands/util/getHeight.ts
+++ src/commands/util/getHeight.ts
@@ -1,20 +1,37 @@
-import {CommandInteraction, SlashCommandBuilder} from "discord.js";
-import {dbSelectRowSingle} from "../../hooks/useDatabase";
+import {ChatInputCommandInteraction, SlashCommandBuilder} from "discord.js";
 import {getPlayerObject} from "../../hooks/useDBObjects";
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('getheight')
-        .setDescription('Replies with current height'),
-    async execute(interaction: CommandInteraction): Promise<void> {
+        .setDescription('Replies with current height')
+        .addBooleanOption( option =>
+        option.setName('share')
+            .setDescription('Share results with the channel?')
+            .setRequired(false)),
+    async execute(interaction: ChatInputCommandInteraction): Promise<void> {
 
-        const userID = interaction.user.id;
-        const guildID = interaction.guild!.id;
+        const userID = String(interaction.user.id);
+        const guildID = String(interaction.guild!.id);
+
+        const shareSize = interaction.options.getBoolean('share', false) ?? false;
+
         try  {
             const player = await getPlayerObject(userID, guildID)
-            await interaction.reply({content: `Your current height is ${player.height} "Generic Height Units ${interaction.member!.user}`, allowedMentions:{parse: []}})
+
+            if (shareSize){
+                await interaction.reply({
+                    content: `${interaction.member!.user}'s current Height is ${player.height} "Generic Height Units `,
+                    allowedMentions: {parse: []}
+                })
+            }else {
+                await interaction.reply({
+                    content: `Your current Height is ${player.height} "Generic Height Units`,
+                    ephemeral: true}
+                )
+            }
         } catch {
-            await interaction.reply('Player Not found')
+            await interaction.reply({content:'Player Not found, have you registered yet?', ephemeral: true})
 
         }
 
